55b607af69ef78629c609f2d71ba48070961c657
BenSotty <ben.sotty@seedrs.com>
2017-12-20 10:52:49 +0000

    Add comments to Api methods

--- lib/salesforce_sync/api.rb
+++ lib/salesforce_sync/api.rb
@@ -1,28 +1,36 @@
 module SalesforceSync
   class Api
+    # Trigger the upsert of a resource to be performed now
     def self.resource_synchronisation(resource)
       SalesforceSync::Resource::QueueItem.new(resource).push_upsert(true)
     end
 
+    # Trigger the upsert of a group of resources
     def self.bulk_synchronisation(ids_by_class)
       SalesforceSync::Bulk::Action.new(ids_by_class: ids_by_class).synchronise
     end
 
+    # Figure out if a resource is synced with Salesforce
     def self.synchronised?(resource)
       sf_resource = SalesforceSync::Resource::Factory.create(resource)
       sf_resource.synchronised?
     end
 
+    # Returns the Salesforce id of the resource
     def self.salesforce_id(resource)
       sf_resource = SalesforceSync::Resource::Factory.create(resource)
       sf_resource.salesforce_id
     end
 
+    # Returns the url of the resource on Salesforce
     def self.url(resource)
       sf_resource = SalesforceSync::Resource::Factory.create(resource)
       sf_resource.url
     end
 
+    # Synchronise all existing resources to Salesforce
+    # Be carreful: it destroys all existing Salesforce ids
+    # Useful in test environment to fill the records of a Sandbox
     def self.platform_reset
       platform_ids_by_class = SalesforceSync.config.resources_by_class.keys.each_with_object(Hash.new{ |h, k| h[k] = [] }) do |klass, ids|
         ids[klass] = klass.pluck(:id)
